ffc8b4407cded01c14c1c96f4da6e290f9218403
ypx9628 <92853636+ypx9628@users.noreply.github.com>
2023-06-27 21:01:08 +0800

    Update ShowGroupMember.java

--- client/src/main/java/com/chatroom/view/ShowGroupMember.java
+++ client/src/main/java/com/chatroom/view/ShowGroupMember.java
@@ -16,6 +16,7 @@ public class ShowGroupMember extends JFrame {
     private JPanel panel1;
     private JLabel label1;
     private JLabel label2;
+    private JLabel label3;
     private DefaultListModel<String> listModel;
     private JList<String> list;
 
@@ -25,9 +26,12 @@ public class ShowGroupMember extends JFrame {
 
         panel1 = new JPanel();
         label1 = new JLabel();
+        label2 = new JLabel();
+        label3 = new JLabel();
         listModel = new DefaultListModel<>();
         list = new JList<>(listModel);
         JScrollPane scrollPane = new JScrollPane(list);
+        scrollPane.setBounds(new Rectangle(new Point(130, 30),scrollPane.getPreferredSize()));
 
         this.setSize(300, 300);
         this.setResizable(false);
@@ -44,8 +48,10 @@ public class ShowGroupMember extends JFrame {
         label1.setBounds(new Rectangle(new Point(50,15), label1.getPreferredSize()));
         container.add(label1);
 
+        label3.setBounds(new Rectangle(new Point(50, 35),label3.getPreferredSize()));
+
         label2.setText("群成员：");
-        label2.setBounds(new Rectangle(new Point(80,30),label2.getPreferredSize()));
+        label2.setBounds(new Rectangle(new Point(130,15),label2.getPreferredSize()));
         container.add(label2);
 
         container.setPreferredSize(new Dimension(400, 180));
@@ -53,11 +59,17 @@ public class ShowGroupMember extends JFrame {
         setLocationRelativeTo(getOwner());
     }
 
+    public void showLeader(List<String> items){
+        label3.setText("");
+        label3.setText(items.get(items.size()-1));
+    }
+
     public void showMembers(List<String> items){
         listModel.clear();
 
-        for(String item: items){
-            listModel.addElement(item);
+        for(int i=0; i<items.size()-1; i++){
+//            listModel.addElement(item);
+            listModel.addElement(items.get(i));
         }
     }
     public static void main(String[] args) {
